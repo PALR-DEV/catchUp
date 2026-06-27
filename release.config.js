e510cfd39d43731b9c68544180e06367a7fcfb8e
Pedro Lorenzo <pedroalorenzo16@gmail.com>
2026-06-26 22:57:26 -0400

    feat: initial release

--- release.config.js
+++ release.config.js
@@ -5,5 +5,9 @@ export default {
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     "@semantic-release/github",
+    ["@semantic-release/git", {
+      assets: ["package.json"],
+      message: "chore: release ${nextRelease.version} [skip ci]"
+    }],
   ],
 };
